1db86f450859d6bbcc546faedf2e666d7b572196
GaborFicsor <ficsorgabor@gmail.com>
2022-10-18 19:24:27 +0000

    Finish divison part

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,7 @@ function runGame(gameType) {
     //Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    let num3 = (num1 * num2);
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -42,6 +43,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num3, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -86,6 +89,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -133,3 +138,9 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "x";
 }
 
+
+function displayDivisionQuestion(operand1, operand2) {
+      document.getElementById("operand1").textContent = (operand1 * operand2);
+      document.getElementById("operand2").textContent = operand2;
+      document.getElementById("operator").textContent = "/";
+}
